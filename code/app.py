4bd6f4356136692514414da98adc7689c1f1278e
Curtis Maloney <curtis@tinbrain.net>
2017-12-25 10:38:30 +1100

    Update app code

--- code/app.py
+++ code/app.py
@@ -1,5 +1,15 @@
 
 def application(env, start_response):
-   start_response('200 OK', [('Content-Type', 'text/plain')])
-   yield b'Welcome!'
+   start_response('200 OK', [('Content-Type', 'text/html')])
+   yield b'''
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Welcome! </title>
+  </head>
+  <body>
+    <p> Welcome! </p>
+  </body>
+</html>'''
+
 
